a5cb55895cc51b0e0af33f110628e704a84ea917
darrenglow <adkadkadk@163.com>
2023-09-12 14:34:59 +0800

    init

---
+++ cmake-build-debug/CMakeFiles/BUAA_Compiler.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "BUAA_Compiler.exe"
+  "BUAA_Compiler.exe.manifest"
+  "BUAA_Compiler.pdb"
+  "CMakeFiles/BUAA_Compiler.dir/main.cpp.obj"
+  "libBUAA_Compiler.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/BUAA_Compiler.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
