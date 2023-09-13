8b4701ed3b97cf761acf6b5c79bbe37c5ecd2485
darrenglow <adkadkadk@163.com>
2023-09-13 16:41:29 +0800

    try gitignore

--- cmake-build-debug/CMakeFiles/BUAA_Compiler.dir/cmake_clean.cmake
+++ cmake-build-debug/CMakeFiles/BUAA_Compiler.dir/cmake_clean.cmake
@@ -1,12 +1,5 @@
-file(REMOVE_RECURSE
-  "BUAA_Compiler.exe"
-  "BUAA_Compiler.exe.manifest"
-  "BUAA_Compiler.pdb"
-  "CMakeFiles/BUAA_Compiler.dir/main.cpp.obj"
-  "libBUAA_Compiler.dll.a"
-)
 
 # Per-language clean rules from dependency scanning.
-foreach(lang CXX)
+foreach(lang )
   include(CMakeFiles/BUAA_Compiler.dir/cmake_clean_${lang}.cmake OPTIONAL)
 endforeach()
